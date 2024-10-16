0754c35f9a4e37ad23fc6c5bd0884812b0920075
Diego.Romel <104534511+DiegoRomel@users.noreply.github.com>
2024-10-16 21:01:37 +0000

    improving data input/output

--- ICBEUPlatformChatBot.py
+++ ICBEUPlatformChatBot.py
@@ -1,17 +1,30 @@
 from telegram import Update
 from telegram.ext import ConversationHandler, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters 
+import requests
 
 
-NATGEO, ANSWER_HANDLING, CAMBRIDGE, PEARSON = range(4)
+CENGAGE, ANSWER_HANDLING, CAMBRIDGE, PEARSON, FOWARD_MESSAGE = range(5)
 
 # start func
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text('Olá! Este é o chat de suporte às plataformas do ICBEU Manaus.\n'
                                     'Com qual plataforma você precisa de ajuda?\n'
-                                    '1 - Natgeo\n'
+                                    '1 - Cengage\n'
                                     '2 - Cambridge\n'
                                     '3 - Pearson\n')
     return ANSWER_HANDLING
+
+
+
+async def forward_message(bot_token, source_chat_id, target_chat_id, message_id):
+    url = f"https://api.telegram.org/bot{bot_token}/forwardMessage"
+    data = {
+        "chat_id": target_chat_id,
+        "from_chat_id": source_chat_id,
+        "message_id": message_id
+    }
+    response = requests.post(url, json=data)
+    return response.json()
 
 
 # answer_handling
@@ -20,18 +33,30 @@ async def answer_handling(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
     print(user_input)
     if user_input == "1":
         print("verificacao certa natgeo")
-        await update.message.reply_text('chose natgeo. Select your problem:\n'
-                                        '1 - I have forgotten my password.\n'
-                                        '2 - I am not in a class.\n'
-                                        '3 - I have a scratched code.\n')
-        return NATGEO
+        await update.message.reply_text('Qual seu problema na plataforma Cengage?\n'
+                                        '1 - Esqueci minha senha.\n'
+                                        '2 - Ainda não estou em uma turma.\n'
+                                        '3 - Código do livro rasurado/inexistente.\n'
+                                        '4 - Não sei meu username.\n'
+                                        '5 - Meu problema não é nenhum desses/meu problema não foi resolvido.\n')
+        return CENGAGE
     elif user_input == "2":
         print("verificacao certa natgeo")
-        await update.message.reply_text('1 - I have lost my email\n'
-                                        '2 - I have switched classes\n'
-                                        '3 - I am a repeat student\n')
+        await update.message.reply_text('Qual seu problema na plataforma da Cambridge?\n'
+                                        '1 - Esqueci minha senha.\n'
+                                        '2 - Ainda não estou em uma turma.\n'
+                                        '3 - Não sei meu username/email.\n'
+                                        '4 - Perdi acesso ao meu email com o qual acessava a plataforma.\n'
+                                        '5 - Não estou na turma/turma errada.\n'
+                                        '6 - Sou aluno repetente.\n'
+                                        '7 - Meu problema não é nenhum desses/meu problema não foi resolvido.\n')
         return CAMBRIDGE
     elif user_input == "3":
+        await update.message.reply_text('Qual seu problema na plataforma da Pearson?\n'
+                                        '1 - Perdi acesso ao meu email com o qual acessava a plataforma.\n'
+                                        '2 - Não estou na turma/turma errada.\n'
+                                        '3 - Sou aluno repetente.\n'
+                                        '4 - Meu problema não é nenhum desses/meu problema não foi resolvido.\n')
         return PEARSON
     else:
         await update.message.reply_text('not a valid option')
@@ -39,14 +64,23 @@ async def answer_handling(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
 
 
 # platforms funcs
-async def natgeo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+async def cengage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_input = update.message.text
     if user_input == "1":
-        await update.message.reply_text('Remember it.')
+        await update.message.reply_text('Se Icbeu123 não funcionar, nos envie o nome do student completo, nome do(a) teacher, dia e horário e em breve a senha será resetada para Icbeu123.')
     elif user_input == "2":
-        await update.message.reply_text('join one.')
+        await update.message.reply_text('Cheque o código da turma no grupo do WhatsApp e insira no campo "course key".')
     elif user_input == "3":
-        await update.message.reply_text('get a new.')
+        await update.message.reply_text('Tire uma foto da página do código rasurado e envie o nome completo do(a) student, nome do(a) teacher e horário da aula. Encaminhe tudo para o teacher.')
+    elif user_input == "4":
+        await update.message.reply_text('Se não estiver na capa do seu livro insira o nome completo do(a) student, nome do(a) teacher e horário da aula, retornaremos o username para o seu contato assim que possível.')
+    elif user_input == "5":
+        try:
+            response = forward_message("7503805557:AAG1BZ3SBN8sdTKRLllgC-XH-WUYVR_Wmr4", "7503805557", "1189922892", "1189922892")
+            print(response)
+        except requests.exceptions.RequestException as err:
+            print('HTTP Request failed: ', err)
+
 
 async def cambridge(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     user_input = update.message.text
@@ -64,7 +98,8 @@ async def pearson(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
 
 #cancel func
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    await ConversationHandler.END
+    await update.message.reply_text('Encerrando conversa.')
+    return ConversationHandler.END
 #app builder
 app = ApplicationBuilder().token("7503805557:AAG1BZ3SBN8sdTKRLllgC-XH-WUYVR_Wmr4").build()
 
@@ -74,9 +109,10 @@ def main() -> None:
         entry_points=[CommandHandler('start', start)],
         states={
             ANSWER_HANDLING: [MessageHandler(filters.TEXT & ~filters.COMMAND, answer_handling)],
-            NATGEO: [MessageHandler(filters.TEXT & ~filters.COMMAND, natgeo)],
+            CENGAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, cengage)],
             CAMBRIDGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, cambridge)],
-            PEARSON: [MessageHandler(filters.TEXT & ~filters.COMMAND, pearson)]
+            PEARSON: [MessageHandler(filters.TEXT & ~filters.COMMAND, pearson)],
+            FOWARD_MESSAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
