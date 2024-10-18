4e0989f324267be5b1d60d0873da7bd27c42246d
Diego.Romel <104534511+DiegoRomel@users.noreply.github.com>
2024-10-18 22:04:43 +0000

    adiciona encaminhamento de mensagem

--- ICBEUPlatformChatBot.py
+++ ICBEUPlatformChatBot.py
@@ -1,9 +1,9 @@
-from telegram import Update
-from telegram.ext import ConversationHandler, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters 
+from telegram import Update, Bot
+from telegram.ext import ConversationHandler, ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
 import requests
 
 
-CENGAGE, ANSWER_HANDLING, CAMBRIDGE, PEARSON, FOWARD_MESSAGE = range(5)
+CENGAGE, ANSWER_HANDLING, CAMBRIDGE, PEARSON, FORWARD_LAST_MESSAGE = range(5)
 
 # start func
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -15,16 +15,11 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     return ANSWER_HANDLING
 
 
-
-async def forward_message(bot_token, source_chat_id, target_chat_id, message_id):
-    url = f"https://api.telegram.org/bot{bot_token}/forwardMessage"
-    data = {
-        "chat_id": target_chat_id,
-        "from_chat_id": source_chat_id,
-        "message_id": message_id
-    }
-    response = requests.post(url, json=data)
-    return response.json()
+async def forward_last_message(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    message = update.effective_message.text
+    print(message)
+    print(update.effective_message.message_id)
+    await bot.forward_message(chat_id=1660064020, from_chat_id=update.effective_chat.id, message_id=update.effective_message.message_id)
 
 
 # answer_handling
@@ -38,7 +33,7 @@ async def answer_handling(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
                                         '2 - Ainda não estou em uma turma.\n'
                                         '3 - Código do livro rasurado/inexistente.\n'
                                         '4 - Não sei meu username.\n'
-                                        '5 - Meu problema não é nenhum desses/meu problema não foi resolvido.\n')
+                                        '5 - Descreva seu problema.\n')
         return CENGAGE
     elif user_input == "2":
         print("verificacao certa natgeo")
@@ -49,19 +44,18 @@ async def answer_handling(update: Update, context: ContextTypes.DEFAULT_TYPE) ->
                                         '4 - Perdi acesso ao meu email com o qual acessava a plataforma.\n'
                                         '5 - Não estou na turma/turma errada.\n'
                                         '6 - Sou aluno repetente.\n'
-                                        '7 - Meu problema não é nenhum desses/meu problema não foi resolvido.\n')
+                                        '7 - Descreva seu problema.\n')
         return CAMBRIDGE
     elif user_input == "3":
         await update.message.reply_text('Qual seu problema na plataforma da Pearson?\n'
                                         '1 - Perdi acesso ao meu email com o qual acessava a plataforma.\n'
                                         '2 - Não estou na turma/turma errada.\n'
                                         '3 - Sou aluno repetente.\n'
-                                        '4 - Meu problema não é nenhum desses/meu problema não foi resolvido.\n')
+                                        '4 - Descreva seu problema.\n')
         return PEARSON
     else:
         await update.message.reply_text('not a valid option')
         return ConversationHandler.END
-
 
 # platforms funcs
 async def cengage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -73,13 +67,10 @@ async def cengage(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     elif user_input == "3":
         await update.message.reply_text('Tire uma foto da página do código rasurado e envie o nome completo do(a) student, nome do(a) teacher e horário da aula. Encaminhe tudo para o teacher.')
     elif user_input == "4":
-        await update.message.reply_text('Se não estiver na capa do seu livro insira o nome completo do(a) student, nome do(a) teacher e horário da aula, retornaremos o username para o seu contato assim que possível.')
+        await update.message.reply_text('Se não estiver na capa do seu livro insira o nome completo do(a) student, nome do(a) teacher e horário da aula, retornaremos o username para o seu contato assim que possível.'),
     elif user_input == "5":
-        try:
-            response = forward_message("7503805557:AAG1BZ3SBN8sdTKRLllgC-XH-WUYVR_Wmr4", "7503805557", "1189922892", "1189922892")
-            print(response)
-        except requests.exceptions.RequestException as err:
-            print('HTTP Request failed: ', err)
+        await update.message.reply_text('Insira: aluno(a), professor(a), dia/horário e descreva seu problema.\n')
+        return FORWARD_LAST_MESSAGE
 
 
 async def cambridge(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -90,11 +81,29 @@ async def cambridge(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
         await update.message.reply_text('join your new class using the class code.')
     elif user_input == "3":
         await update.message.reply_text('get a new.')
+    elif user_input == "4":
+        await update.message.reply_text('get a new.')
+    elif user_input == "5":
+        await update.message.reply_text('get a new.')
+    elif user_input == "6":
+        await update.message.reply_text('get a new.')
+    elif user_input == "7":
+        await update.message.reply_text('Insira: aluno(a), professor(a), dia/horário e descreva seu problema.\n')
+        return FORWARD_LAST_MESSAGE
 
 
 async def pearson(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
-    print('user chose pearson')
-    await update.message.reply_text('chose pearson')
+    user_input = update.message.text
+    if user_input == "1":
+        await update.message.reply_text('Find it.')
+    elif user_input == "2":
+        await update.message.reply_text('join your new class using the class code.')
+    elif user_input == "3":
+        await update.message.reply_text('get a new.')
+    elif user_input == "4":
+        await update.message.reply_text('Insira: aluno(a), professor(a), dia/horário e descreva seu problema.\n')
+        return FORWARD_LAST_MESSAGE
+
 
 #cancel func
 async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -102,6 +111,8 @@ async def cancel(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     return ConversationHandler.END
 #app builder
 app = ApplicationBuilder().token("7503805557:AAG1BZ3SBN8sdTKRLllgC-XH-WUYVR_Wmr4").build()
+bot = Bot("7503805557:AAG1BZ3SBN8sdTKRLllgC-XH-WUYVR_Wmr4")
+
 
 # main func
 def main() -> None:
@@ -112,7 +123,7 @@ def main() -> None:
             CENGAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, cengage)],
             CAMBRIDGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, cambridge)],
             PEARSON: [MessageHandler(filters.TEXT & ~filters.COMMAND, pearson)],
-            FOWARD_MESSAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message)]
+            FORWARD_LAST_MESSAGE: [MessageHandler(filters.TEXT & ~filters.COMMAND, forward_last_message)]
         },
         fallbacks=[CommandHandler('cancel', cancel)]
     )
